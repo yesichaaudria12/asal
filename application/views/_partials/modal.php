dcdb6b7cac21551be589f75c7f02a4e42e31020b
Yesicha Audria <yesichaaudria122@gmail.com>
2024-01-30 14:44:59 +0700

    up

--- application/views/_partials/modal.php
+++ application/views/_partials/modal.php
@@ -43,7 +43,7 @@
           <div class="modal-dialog" role="document">
               <div class="modal-content">
                   <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Add Account Mentor</h5>
+                      <h5 class="modal-title" id="exampleModalLabel">Add New Account</h5>
                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                       </button>
@@ -207,7 +207,8 @@
                       </form>
 
                       <?php } else { ?>
-                      Maaf, Anda sudah mengajukan permohonan Prakerin sebelumnya, silahkan menunggu validasi permohonan Prakerin
+                      Maaf, Anda sudah mengajukan permohonan Prakerin sebelumnya, silahkan menunggu validasi permohonan
+                      Prakerin
                       dari Koordinator Prakerin.
                       <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
