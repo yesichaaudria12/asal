352af0f985d58ac963e6718de66f750dde31644c
unknown <rendifauzan0987@gmail.com>
2024-02-07 20:34:01 +0700

    update terbaru

--- application/views/profile/myprofile.php
+++ application/views/profile/myprofile.php
@@ -32,32 +32,82 @@
                         <h1 class="h2 mr-4 mb-0 text-gray-800"><?php echo $title ?></h1>
                     </div>
 
-                    <!-- Total Pengajuan Content -->
+                    <!-- My Profile Content -->
+                    <div class="container">
+                        <div class="main-body">
+                            <div class="row">
+                                <div class="col-lg-12 mb-4">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="align-items-center text-center">
+                                                <img src="<?php echo base_url('assets/img/profile.jpg'); ?>" alt="" class="rounded-circle p-1 bg-primary" width="110">
+                                                <h4 class="card-title"><?php echo $user_profile->username; ?></h4>
+                                                <h6 class="card-title"><?php echo $user_profile->role; ?></h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <form action="<?= current_url(); ?>" method="POST">
+                                                <div class="row mb-3">
+                                                    <div class="col-sm-3">
+                                                        <h6 class="mb-0">Username</h6>
+                                                    </div>
+                                                    <div class="col-sm-9 text-secondary">
+                                                        <input type="text" class="form-control" name="username" value="<?= $user_profile->username; ?>" />
+                                                        <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
+                                                    </div>
+                                                </div>
+                                                <div class="row mb-3">
+                                                    <div class="col-sm-3">
+                                                        <h6 class="mb-0">Password</h6>
+                                                    </div>
+                                                    <div class="col-sm-9 text-secondary">
+                                                        <input type="text" class="form-control" name="password" value="<?= $user_profile->password; ?>" />
+                                                        <?= form_error('password', '<small class="text-danger">', '</small>'); ?>
+                                                    </div>
+                                                </div>
 
+                                                <div class="row">
+                                                    <div class="col-sm-3"></div>
+                                                    <div class="col-sm-9 text-secondary">
+                                                        <button type="submit" class="btn btn-primary px-4">Ubah</button>
+                                                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#password">Ubah Password</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End of Main Content -->
 
                 </div>
-                <!-- End of Main Content -->
+                <!-- End of Content Wrapper -->
 
             </div>
-            <!-- End of Content Wrapper -->
+            <!-- End of Page Wrapper -->
 
         </div>
-        <!-- End of Page Wrapper -->
+        <!-- Footer -->
+        <?php $this->load->view("_partials/footer.php") ?>
+        <!-- End of Footer -->
 
-    </div>
-    <!-- Footer -->
-    <?php $this->load->view("_partials/footer.php") ?>
-    <!-- End of Footer -->
+        <!-- Scroll to Top Button-->
+        <?php $this->load->view("_partials/scrolltop.php") ?>
 
-    <!-- Scroll to Top Button-->
-    <?php $this->load->view("_partials/scrolltop.php") ?>
+        <!-- Logout Modal-->
+        <?php $this->load->view("_partials/modal.php") ?>
 
-    <!-- Logout Modal-->
-    <?php $this->load->view("_partials/modal.php") ?>
-
-    <!-- Bootstrap core JavaScript-->
-    <?php $this->load->view("_partials/js.php") ?>
+        <!-- Bootstrap core JavaScript-->
+        <?php $this->load->view("_partials/js.php") ?>
 
 </body>
+
+
 
 </html>
